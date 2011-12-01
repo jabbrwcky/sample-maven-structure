d6fad20633ee2db34c871df938606c1d13e7031f
Jens Hausherr <jabbrwcky@googlemail.com>
2011-12-01 22:31:31 +0100

    First commit

---
+++ module-b/src/main/java/com/example/HelloWorldB.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jens
+ * Date: 01.12.11
+ * Time: 21:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldB extends HelloWorldA {
+
+    public static void main(String... args){
+        System.out.println("Hello, World B");
+    }
+}
